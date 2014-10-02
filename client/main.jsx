174e8f18340a8126b016228cf6a6125ccb266296
g1l3sp <g1l3sp@gmail.com>
2014-10-02 11:31:32 -0400

    * very questionable UI progress * model tweaks

--- client/main.jsx
+++ client/main.jsx
@@ -6,6 +6,154 @@ var React = require('react');
 
 /** @jsx React.DOM */
 
+/* A selector for degrees */
+var DegreeSelect = React.createClass({
+    loadDegrees: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({degreeData: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    getInitialState: function() {
+        return {degreeData: []};
+    },
+    componentDidMount: function() {
+        this.loadDegrees();
+    },
+    render: function() {
+        var degreeNodes = this.state.degreeData.map(function(degree, index) {
+            return (
+                <option value={degree.id}>{degree.program}</option>
+            );
+        });
+        return (
+            <div>
+            <select className="form-control" name="degree">
+                {degreeNodes}
+            </select>
+            </div>
+        );
+    }
+});
+
+var AuditForm = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var degreeId = this.refs.degree.getDOMNode().value.trim();
+        if (!degreeId) {
+            return;
+        }
+        this.props.onCourseSubmit({degreeId: degreeId});
+        this.refs.degree.getDOMNode().value = '';
+        return;
+    },
+    render: function() {
+        return (
+            <form className="auditForm" role="form" onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="degree">Select program</label>
+                    <DegreeSelect url="degrees/" />
+                </div>
+                <button type="submit" className="btn btn-default">Submit</button>
+            </form>
+        );
+    }
+});
+
+var AuditResults = React.createClass({
+    loadAudit: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        this.loadAudit();
+    },
+    render: function() {
+        var metNodes = "";
+        if (this.state.data.metRequirements) {
+            metNodes = this.state.data.metRequirements.map(function(req, index) {
+                var metByNodes = req.metby.map(function(courseId, index) {
+                    return (
+                        <li> {courseId} </li>
+                    );
+                });
+                return (
+                        <li> <h3>{req.id}</h3>
+                            met by:
+                            <ul>
+                            {metByNodes}
+                            </ul>
+                        </li>
+                );
+            });
+        }
+
+        var unmetNodes = "";
+        if (this.state.data.unmetRequirements) {
+            unmetNodes = this.state.data.unmetRequirements.map(function(req, index) {
+                var metByNodes = req.metby.map(function(courseId, index) {
+                    return (
+                        <li> {courseId} </li>
+                    );
+                });
+                return (
+                    <li> <h3>{req.id}</h3>
+                        partially met by:
+                        <ul>
+                            {metByNodes}
+                        </ul>
+                    </li>
+                );
+            });
+        }
+
+
+        return (
+            <div>
+                <h1 className="page-header">Results</h1>
+                <h2>Met Requirements</h2>
+                <ul>
+                {metNodes}
+                </ul>
+                <h2>Requirements Unmet</h2>
+                <ul>
+                {unmetNodes}
+                </ul>
+            </div>
+        );
+    }
+});
+
+var AuditBox = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <AuditForm />
+                <AuditResults url="audit/degrees/BS-CS/students/1234567890" />
+            </div>
+        );
+    }
+});
+
+
+
 /*
  * The details of a Course.
  */
@@ -108,7 +256,19 @@ var CourseBox = React.createClass({
     }
 });
 
+//React.renderComponent(
+//    <CourseBox url="courses/" />,
+//    document.body
+//);
+
 React.renderComponent(
-    <CourseBox url="courses/" />,
-    document.body
+    <AuditBox/>
+    ,
+    document.getElementById('auditBox')
 );
+
+//React.renderComponent(
+//    <AuditResults/>
+//    ,
+//    document.getElementById('auditResults')
+//);
--- server/audit.js
+++ server/audit.js
@@ -10,6 +10,7 @@ exports.report = function(studentId, degreeId) {
 
     var sampleAudit = {
         "studentId" : "1234567890",
+        "degreeId" : "BS-CS",
             "metRequirements" : [
             {
                 "id" : "CS-CAPS",
@@ -31,6 +32,10 @@ exports.report = function(studentId, degreeId) {
     return new Promise(function(resolve, reject) {
         if (studentId != sampleAudit.studentId) {
             reject("The Audit Service is currently unavailable for this student");
+        }
+
+        if (degreeId != sampleAudit.degreeId) {
+            reject("The Audit Service is currently unavailable for this degree");
         }
 
         resolve(sampleAudit);
